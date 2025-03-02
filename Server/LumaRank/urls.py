e2bd7247a2c4446422ad6b48a52d60d8b65e7791
StarGazer04 <william@bwcarlson.com>
2025-03-02 17:05:00 -0600

    added static files

--- Server/LumaRank/urls.py
+++ Server/LumaRank/urls.py
@@ -6,6 +6,15 @@ from . import views #from current directory import views - imports views.py
 # the first arg means this pattern matches root of URL
 # second arg views.index specifies dthat when this url is accessd, django shoudl call index function from views
 # third arg specifies pattern name, so it can be referenced later
+
+#Explanation: Application sends django a GET request. Django knows its inside the LumaRank project, so it loos
+#             in the LumaRank urls. it then comapres teh name field with the GET requested field. if they match then
+#             Django addds the url string (first param) to the global URL.
 urlpatterns = [
-    path("", views.index, name="index") ,
+    path("", views.main, name="main") ,
+    path("login/", views.LoginPage, name="LoginPage"), 
+    path("logout/", views.logout_view, name="logout"), 
+    path("register/", views.RegisterPage, name="RegisterPage"),
+    path("search/", views.SearchPage, name="SearchPage"),
+
 ]
--- Server/LumaRank/views.py
+++ Server/LumaRank/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 
-def index(request):
+def main(request):
     return render(request, 'main.html')
+
+#login page
+def LoginPage(request):
+    return render(request, 'Login.html')
+
+#logout process: need to ensure users are logged out before returning them to login page
+def logout_view(request): 
+    return redirect("LoginPage")
+
+#register page
+def RegisterPage(request):
+    return render(request, "Register.html")
+
+#search page
+def SearchPage(request):
+    return render(request, 'Search.html')
+
